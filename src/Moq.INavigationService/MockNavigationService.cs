af0eee6aae95d31ffff23f96b81cf8ec63f17f27
Axemasta <33064621+Axemasta@users.noreply.github.com>
2024-04-03 15:44:58 +0100

    Updated Nugets
    All of the nugets are now on the latest and greatest. Fixed the MockNavigationService missing some bits from Prism 9

--- src/Moq.INavigationService/MockNavigationService.cs
+++ src/Moq.INavigationService/MockNavigationService.cs
@@ -3,78 +3,88 @@ namespace Moq;
 
 public class MockNavigationService : Mock<INavigationService>, INavigationService, IRegistryAware
 {
-    public bool UsingNavigationBuilder { get; set; }
+	public bool UsingNavigationBuilder { get; set; }
 
-    public MockNavigationService()
-    {
-        Registry = new MockViewRegistry();
-    }
+	public MockNavigationService()
+	{
+		Registry = new MockViewRegistry();
+	}
 
-    #region INavigationService
+	#region INavigationService
 
-    public Task<INavigationResult> GoBackAsync(INavigationParameters parameters)
-    {
-        return Object.GoBackAsync(parameters);
-    }
+	public Task<INavigationResult> GoBackAsync(INavigationParameters parameters)
+	{
+		return Object.GoBackAsync(parameters);
+	}
 
-    public Task<INavigationResult> GoBackToAsync(string name, INavigationParameters parameters)
-    {
-        return Object.GoBackToAsync(name, parameters);
-    }
+	public Task<INavigationResult> GoBackAsync(string viewName, INavigationParameters parameters)
+	{
+		return Object.GoBackAsync(viewName, parameters);
+	}
 
-    public Task<INavigationResult> GoBackToRootAsync(INavigationParameters parameters)
-    {
-        return Object.GoBackToRootAsync(parameters);
-    }
+	public Task<INavigationResult> GoBackToAsync(string name, INavigationParameters parameters)
+	{
+		return Object.GoBackToAsync(name, parameters);
+	}
 
-    public Task<INavigationResult> NavigateAsync(Uri uri, INavigationParameters parameters)
-    {
-        return Object.NavigateAsync(uri, parameters);
-    }
+	public Task<INavigationResult> GoBackToRootAsync(INavigationParameters parameters)
+	{
+		return Object.GoBackToRootAsync(parameters);
+	}
 
-    #endregion INavigationService
+	public Task<INavigationResult> NavigateAsync(Uri uri, INavigationParameters parameters)
+	{
+		return Object.NavigateAsync(uri, parameters);
+	}
 
-    #region IRegistryAware
+	public Task<INavigationResult> SelectTabAsync(string name, INavigationParameters parameters)
+	{
+		return Object.SelectTabAsync(name, parameters);
+	}
 
-    public IViewRegistry Registry { get; }
+	#endregion INavigationService
 
-    #endregion IRegistryAware
+	#region IRegistryAware
 
-    private class MockViewRegistry : IViewRegistry
-    {
-        public IEnumerable<ViewRegistration> Registrations { get; } = new List<ViewRegistration>
-        {
-            new ViewRegistration
-            {
+	public IViewRegistry Registry { get; }
+
+	#endregion IRegistryAware
+
+	private class MockViewRegistry : IViewRegistry
+	{
+		public IEnumerable<ViewRegistration> Registrations { get; } = new List<ViewRegistration>
+		{
+			new ViewRegistration
+			{
                 // Otherwise we run into this exception:
                 // https://github.com/PrismLibrary/Prism/blob/d8d47b8465038c18503761aaba6a68a9bdf35a0c/src/Maui/Prism.Maui/Navigation/Builder/NavigationBuilderExtensions.cs#L88C16-L88C16
                 View = typeof(NavigationPage),
-            },
-        };
+			},
+		};
 
-        public object CreateView(IContainerProvider container, string name)
-        {
-            return name;
-        }
+		public object CreateView(IContainerProvider container, string name)
+		{
+			return name;
+		}
 
-        public string GetViewModelNavigationKey(Type viewModelType)
-        {
-            return viewModelType.Name;
-        }
+		public string GetViewModelNavigationKey(Type viewModelType)
+		{
+			return viewModelType.Name;
+		}
 
-        public Type GetViewType(string name)
-        {
-            return name.GetType();
-        }
+		public Type GetViewType(string name)
+		{
+			return name.GetType();
+		}
 
-        public bool IsRegistered(string name)
-        {
-            return true;
-        }
+		public bool IsRegistered(string name)
+		{
+			return true;
+		}
 
-        public IEnumerable<ViewRegistration> ViewsOfType(Type baseType)
-        {
-            return Registrations;
-        }
-    }
+		public IEnumerable<ViewRegistration> ViewsOfType(Type baseType)
+		{
+			return Registrations;
+		}
+	}
 }
